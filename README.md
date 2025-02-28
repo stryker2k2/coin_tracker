f6befce0f63ace599a6f0341a585c20d31f937aa
Jack Ward <stryker2k2@msn.com>
2025-02-27 20:18:13 -0600

    Linting & README Updates

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-### COINBASE TRADING
+# Coin Tracker
 
-Using Coinbase Advanced API Python SDK which can be found at:
+Using Coinbase Advanced API Python SDK which can be found at:\
 https://github.com/coinbase/coinbase-advanced-py
+
+## Linting
+``` bash
+# https://pypi.org/project/ruff/
+ruff check . --fix
+```
+
+## Installing
+``` bash
+# Windows
+python3 -m pip install virtualenv
+python3 -m virtualenv venv
+.\venv\Scripts\activate
+(venv) pip install -r requirements.txt
+
+# Linux
+python3 -m pip install virtualenv
+python3 -m virtualenv venv
+source ./venv/bin/activate
+(venv) pip install -r requirements.txt
+```
+
+## Running
+``` bash
+# Windows
+(venv) python app.py
+```
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from coinbase.rest import RESTClient
-import json, uuid
+import json
+import uuid
+import os
 
 client = RESTClient(key_file="./keys/cdp_api_key.json")
 
 # Get Accounts
 def get_accts():
     accounts = client.get_accounts()
+    os.makedirs("output/accounts", exist_ok=True)
     filepath = "./output/accounts/accounts.json"
     with open(filepath, "w") as f:
         json.dump(accounts.to_dict(), f, indent=4)
@@ -14,7 +17,6 @@ def get_accts():
 # Get Account Amount
 def get_assets():
     accounts = client.get_accounts()
-    filter_acct = "XRP"
     print("\nWallet")
     for acct in accounts.accounts:
         if float(acct.available_balance['value']) > 0:
@@ -22,7 +24,7 @@ def get_assets():
                 print(f"{acct.currency}: {float(acct.available_balance['value']):.2f}")
             else:
                 product = client.get_product(f"{acct.currency}-USDC")
-                print(f"{acct.currency}-USDC: {float(acct.available_balance['value']):.2f} (${(float(product.price) * float(acct.available_balance['value'])):.2f})")
+                print(f"{acct.currency}: {float(acct.available_balance['value']):.2f} (${(float(product.price) * float(acct.available_balance['value'])):.2f})")
 
 # Get Product & Print Price
 def get_stock_price():
@@ -30,31 +32,36 @@ def get_stock_price():
     product = client.get_product(my_products[4])
     print("\nMarket")
     print(f"{product.product_id}: ${product.price} / coin")
+    os.makedirs("output/products", exist_ok=True)
     with open(f"./output/products/{product.product_id}.json", "w") as f:
         json.dump(product.to_dict(), f, indent=4)
 
 # Buy Coin(s)
 def buy_coins():
     order_id = str(uuid.uuid4())
-    response = input(f"\nDo you want to make a purchase (y/n)? ")
+    response = input("\nDo you want to make a purchase (y/n)? ")
     if response != "y":
         print("\nPurchase canceled\n")
         return
-    product = input(f"\nWhat stock do you want to purchase (ex: BTC-USDC)? ")
+    product = input("\nWhat stock do you want to purchase (ex: BTC-USDC)? ")
     response = input(f"\nHow many {product} do you want to buy (in Dollars)? ")
     order_size = str(response)
-    response = input(f"\nDo you want to buy {order_size} of {product} coins (y/n)? ")
+    response = input(f"\nDo you want to buy ${order_size} of {product} coins (y/n)? ")
     if response == "y":
         order = client.market_order_buy(client_order_id=order_id, product_id=product, quote_size=order_size)
-        print(json.dumps(order.to_dict(), indent=2))
+        os.makedirs("output/orders", exist_ok=True)
         filepath = f"./output/orders/{product}-{order_id}.json"
         with open(filepath, "w") as f:
             json.dump(order.to_dict(), f, indent=4)
-        print(f"\nJSON Output: {filepath}\n")
+        if order.success:
+            print(f"\nJSON Output: {filepath}\n")
+        else:
+            print("\n[!] Purchase Failed\n")
     else:
         print("\nPurchase canceled\n")
 
 # get_accts()
 # get_stock_price()
 get_assets()
-# buy_coins()
+buy_coins()
+print("\n")
